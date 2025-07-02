8a99ced45d1048f9f170a3da06e2311c1bd56c4b
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2025-07-02 21:27:25 +0200

    Disclaimer WEATHER_API_KEY public

--- front-end/generate-env.sh
+++ front-end/generate-env.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-WEATHER_API_KEY=5ccad1686b1549b992d70017250602
+WEATHER_API_KEY=5ccad1686b1549b992d70017250602 # Ne devrait pas etre hardcode, mais ca l'etait deja dans le projet initial
 FILE_DIR=src/environments/
 FILE_NAME=environment.ts
 FILE=$FILE_DIR$FILE_NAME
