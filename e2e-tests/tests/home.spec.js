3f6a83cab98f7589a7db0a472473ef4614a835f0
Minael Trumacher <127477922+MinaelTrumacher@users.noreply.github.com>
2025-07-02 21:50:59 +0200

    Mise en place e2e

---
+++ e2e-tests/tests/home.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('la page d’accueil affiche le titre', async ({ page }) => {
+    await page.goto('http://localhost:4200');
+    await expect(page).toHaveTitle(/todo/i);
+});
+
