03d216809e75cc6889bca00195f6ee558e0bbf30
Minael Trumacher <127477922+MinaelTrumacher@users.noreply.github.com>
2025-07-02 22:49:41 +0200

    Ajout script test:e2e

--- e2e-tests/tests/home.spec.js
+++ e2e-tests/tests/home.spec.js
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('la page d’accueil affiche le titre', async ({ page }) => {
     await page.goto('http://localhost:4200');
-    await expect(page).toHaveTitle(/todo/i);
+    await expect(page).toHaveTitle(/todo/i); // Test
 });
 
